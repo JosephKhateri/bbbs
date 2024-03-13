8cc05b43654a7800c2fcddb77f822c9d13122e84
mmeiser <mmeiser19@gmail.com>
2024-03-12 22:06:25 -0400

    Currently working on Add user functionality. It is almost done but there are some kinks that need to be worked out

--- domain/User.php
+++ domain/User.php
@@ -59,7 +59,7 @@ class User {
         else
             $this->password = $pass;*/
         //$this->password = $password; // need to work on the above stuff with password hashing
-        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        $this->password = password_hash($password, PASSWORD_BCRYPT); //using this for now
     }
 
     function get_id() {
