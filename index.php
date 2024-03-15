9e3a7f57c300a6cc1456b4b0861b8175c933ca44
Zack Cherry <zackcherry2316@gmail.com>
2024-03-15 19:24:21 -0400

    Removed unnessecary files

--- index.php
+++ index.php
@@ -71,38 +71,7 @@
                         }
                     ?></span>
                 </div>
-                <!--<div class="dashboard-item" data-link="calendar.php">
-                    <img src="images/view-calendar.svg">
-                    <span>View Calendar</span>
-                </div>-->
-                <?php if ($_SESSION['access_level'] >= 2): ?>
-                    <!--<div class="dashboard-item" data-link="addEvent.php">
-                        <img src="images/new-event.svg">
-                        <span>Add Appointment</span>
-                    </div>-->
-                <?php endif ?>
-				<!--<div class="dashboard-item" data-link="addAnimal.php">
-                    <img src="images/settings.png">
-                    <span>Add Animal</span>
-                </div>
-				<div class="dashboard-item" data-link="addService.php">
-                    <img src="images/settings.png">
-                    <span>Add Service</span>
-                </div>
-				<div class="dashboard-item" data-link="addLocation.php">
-                    <img src="images/settings.png">
-                    <span>Add Location</span>
-                </div>
-                <div class="dashboard-item" data-link="findAnimal.php">
-                        <img src="images/person-search.svg">
-                        <span>Find Animal</span>
-                </div>-->
-                <!-- Commenting out because volunteers won't be searching events
-                <div class="dashboard-item" data-link="eventSearch.php">
-                    <img src="images/search.svg">
-                    <span>Find Event</span>
-                </div>
-                -->
+
                 <div class="dashboard-item" data-link="UploadForm.php">
                     <img src="images/volunteer-history.svg">
                     <span>Upload File</span>
@@ -111,25 +80,12 @@
                     <img src="images/create-report.svg">
                     <span>Create Report</span>
                 </div>
-                <!--<?php if ($_SESSION['access_level'] >= 2): ?>
-                    <div class="dashboard-item" data-link="personSearch.php">
-                        <img src="images/person-search.svg">
-                        <span>Find Volunteer</span>
-                    </div>
-                    <div class="dashboard-item" data-link="register.php">
-                        <img src="images/add-person.svg">
-                        <span>Register Volunteer</span>
-                    </div>
-                    <div class="dashboard-item" data-link="viewArchived.php">
-                        <img src="images/person-search.svg">
-                        <span>Archived Animals</span>
-                    </div>-->
+                <?php if ($_SESSION['access_level'] >= 2): ?>
                     <!--***added User Registration button ?***-->
                     <div class="dashboard-item" data-link="registerUserForm.php">
                         <img src="images/settings.png">
                         <span>Add User</span>
                     </div>
-
                 <?php endif ?>
                 <?php if ($notRoot) : ?>
                     <div class="dashboard-item" data-link="viewProfile.php">
@@ -140,12 +96,6 @@
                         <img src="images/manage-account.svg">
                         <span>Edit Profile</span>
                     </div>
-                <?php endif ?>
-                <?php if ($notRoot) : ?>
-                    <!--<div class="dashboard-item" data-link="volunteerReport.php">
-                        <img src="images/volunteer-history.svg">
-                        <span>View My Hours</span>
-                    </div>-->
                 <?php endif ?>
                 <div class="dashboard-item" data-link="changePassword.php">
                     <img src="images/change-password.svg">
