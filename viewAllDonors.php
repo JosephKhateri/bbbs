55dbb9b1277d6a4aa4b4a448c2bf7b25a6596622
mmeiser <mmeiser19@gmail.com>
2024-04-07 17:00:01 -0400

    Adding sorting donors by column header

--- viewAllDonors.php
+++ viewAllDonors.php
@@ -128,6 +128,7 @@
         }*/
     </style>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
 <body>
@@ -162,10 +163,9 @@
                 border: 1px solid #333333;
                 text-align: left;
                 padding: 8px;
-                font-weight: 500;
-            }
-            footer {
-                margin-bottom: 5rem;
+                font-weight: bold;
+                text-decoration: underline;
+                cursor: pointer;
             }
         </style>
 
@@ -176,7 +176,7 @@
         <script>
             $(document).ready(function() {
                 $('#searchInput').on('input', function() {
-                    var query = $(this).val();
+                    let query = $(this).val();
 
                     $.ajax({
                         url: 'viewAllDonors.php',
@@ -195,10 +195,10 @@
         <!-- When a donor is clicked, then will redirect to viewDonor.php with that donor's email passed as a parameter -->
         <table id="donorTable">
             <tr>
-                <th>Email</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Company</th>
+                <th onclick="sortTable(0)">Email</th>
+                <th onclick="sortTable(1)">First Name</th>
+                <th onclick="sortTable(2)">Last Name</th>
+                <th onclick="sortTable(3)">Company</th>
             </tr>
             <?php
                 foreach ($donors as $donor) {
@@ -211,6 +211,63 @@
                 }
             ?>
         </table>
+
+        <script>
+            function sortTable(n) {
+                let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+                table = document.getElementById("donorTable");
+                switching = true;
+                // Set the sorting direction to ascending:
+                dir = "asc";
+                /* Make a loop that will continue until
+                no switching has been done: */
+                while (switching) {
+                    // Start by saying: no switching is done:
+                    switching = false;
+                    rows = table.rows;
+                    /* Loop through all table rows (except the
+                    first, which contains table headers): */
+                    for (i = 1; i < (rows.length - 1); i++) {
+                        // Start by saying there should be no switching:
+                        shouldSwitch = false;
+                        /* Get the two elements you want to compare,
+                        one from current row and one from the next: */
+                        x = rows[i].getElementsByTagName("TD")[n];
+                        y = rows[i + 1].getElementsByTagName("TD")[n];
+                        /* Check if the two rows should switch place,
+                        based on the direction, asc or desc: */
+                        if (dir === "asc") {
+                            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                                // If so, mark as a switch and break the loop:
+                                shouldSwitch = true;
+                                break;
+                            }
+                        } else if (dir === "desc") {
+                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                                // If so, mark as a switch and break the loop:
+                                shouldSwitch = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (shouldSwitch) {
+                        /* If a switch has been marked, make the switch
+                        and mark that a switch has been done: */
+                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                        switching = true;
+                        // Each time a switch is done, increase this count by 1:
+                        switchcount ++;
+                    } else {
+                        /* If no switching has been done AND the direction is "asc",
+                        set the direction to "desc" and run the while loop again. */
+                        if (switchcount === 0 && dir === "asc") {
+                            dir = "desc";
+                            switching = true;
+                        }
+                    }
+                }
+            }
+        </script>
 
         <br>
         <!-- Button to export donor information to a CSV file -->
